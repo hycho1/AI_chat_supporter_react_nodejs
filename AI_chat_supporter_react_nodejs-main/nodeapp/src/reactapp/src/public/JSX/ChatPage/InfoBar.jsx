ba77aa4830c5da22da661fda1770e08f00b6cc76
juniyan9 <hjoo111@naver.com>
2024-08-09 19:30:27 +0900

    정리

--- AI_chat_supporter_react_nodejs-main/nodeapp/src/reactapp/src/public/JSX/ChatPage/InfoBar.jsx
+++
@@ -1,9 +0,0 @@
-import '../../CSS/InfoBar.css';
-
-export default function InfoBar({roomName}) {
-    return (
-        <div className="InfoBar">
-            <h2> {roomName}입니다 </h2>
-        </div>
-    )
-}
--- AI_chat_supporter_react_nodejs-main/nodeapp/src/server.js
+++
@@ -1,9 +0,0 @@
-import { app } from './app.js';
-
-const PORT = 5000;
-
-// 서버 생성 및 실행
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
